9ec5528331e85a3ca49293e8bcd93d2bed11b2d8
MitchZas <mitchellzasa@gmail.com>
2026-05-14 09:34:12 -0400

    Sprite will change color when clicked

--- Incremental Game 1/Assets/Scripts/InputHandler.cs
+++ Incremental Game 1/Assets/Scripts/InputHandler.cs
@@ -1,22 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
     private Camera _mainCamera;
+    private bool isClicked;
+    [SerializeField] SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _mainCamera = Camera.main;
+        isClicked = false;
+
+        Vector3 scaleUp = new Vector3(4, 4, 4);
     }
 
     public void OnClick(InputAction.CallbackContext context)
     {
+        isClicked = true;
         if (!context.started) return;
 
         var rayhit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
         if (!rayhit.collider) return;
 
         Debug.Log(rayhit.collider.gameObject.name);
+    }
+
+    void Update()
+    {
+        if (isClicked)
+        {
+            _spriteRenderer.color = Color.red;
+        }
     }
 }
