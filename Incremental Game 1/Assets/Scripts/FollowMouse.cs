3ffb13e141fba78b613bf9999d79a74f54a58212
MitchZas <mitchellzasa@gmail.com>
2026-05-15 17:53:26 -0400

    Added the brush to the middle of the screen and hid the cursor

--- Incremental Game 1/Assets/Scripts/FollowMouse.cs
+++ Incremental Game 1/Assets/Scripts/FollowMouse.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
+    private Camera mainCamera;
+
+    [SerializeField] private float maxSpeed = 10f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FollowMousePosition();
+        Cursor.visible = false;
+    }
+
+    private void FollowMousePosition()
+    {
+        transform.position = GetWorldPositionFromMouse();
+    }
+
+    private Vector2 GetWorldPositionFromMouse()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = -mainCamera.transform.position.z;
+        return mainCamera.ScreenToWorldPoint(mousePos);
+
+        //return mainCamera.ScreenToWorldPoint(Input.mousePosition);
     }
 }
